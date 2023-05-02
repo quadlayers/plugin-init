669eb69c93f7e50c5ad9dd73c3424fac2cf492bc
Martin Meza <martinmezacorreo@gmail.com>
2023-05-02 14:15:59 -0300

    fix deleted zip script

--- bin/deleteZip.js
+++ bin/deleteZip.js
@@ -1,25 +1,18 @@
 const fs = require('fs');
 const { consoleSuccess } = require('./helpers');
 
-// Get arguments
-const myArgs = process.argv.slice(2);
+const args = process.argv.slice(2);
 
-// For each argument/folder
-myArgs.forEach((element) => {
-  // Check if it's a directory
-  fs.stat(element, (err, stats) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-
-    if (stats.isDirectory()) {
-      // Delete only if the directory ends with .zip
-      if (element.endsWith('.zip')) {
-        fs.rm(element, { recursive: true }, (err) => {
-          err ?? consoleSuccess(`${element} deleted`);
-        });
+// For each argument/file
+args.forEach((file) => {
+  // Delete only if the file ends with .zip
+  if (file.endsWith('.zip')) {
+    fs.rm(file, { recursive: true }, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        consoleSuccess(`${file} deleted`);
       }
-    }
-  });
+    });
+  }
 });
