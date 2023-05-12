5b5d3124ea2798423181b4c2240b3039dfa314f1
Francisco <6652831+franmastromarino@users.noreply.github.com>
2023-05-12 13:59:10 -0300

    woocommerce version

--- plugin-init.php
+++ plugin-init.php
@@ -11,10 +11,10 @@
  * License:                 GPLv3
  * Domain Path:             /languages
  * Request at least:        4.7.0
- * Tested up to:            6.1
+ * Tested up to:            6.2
  * Requires PHP:            5.6
  * WC requires at least:    4.0
- * WC tested up to:         7.4
+ * WC tested up to:         7.7
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
